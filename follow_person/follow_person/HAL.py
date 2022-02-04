3d119ed611b5decdc4df5c8bc75a79ebcc711f23
Carlos Caminero <carlos2caminero@gmail.com>
2022-02-04 17:42:07 +0100

    adding nodes follow person (python)

--- follow_person/follow_person/HAL.py
+++ follow_person/follow_person/HAL.py
@@ -4,7 +4,8 @@ import geometry_msgs.msg
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from std_msgs.msg import String
-
+from darknet_ros_msgs.msg import BoundingBoxes, ObjectCount
+from sensor_msgs.msg import LaserScan
 
 class VelocityPublisher(Node):
 
@@ -35,9 +36,70 @@ class VelocityPublisher(Node):
         self.pub.publish(msg)
 
 
+class DarknetRos(Node):
+    def __init__(self):
+        super().__init__("darknet_ros_node")
+        
+        self.bounding_boxes_susbscription = self.create_subscription(
+            BoundingBoxes,
+            'darknet_ros/bounding_boxes',
+            self.boundingBoxesCallback,
+            10)
+        self.found_object_subscription = self.create_subscription(
+            ObjectCount,
+            'darknet_ros/found_object',
+            self.foundObjectCallback,
+            10
+        )
+
+        self.bounding_boxes_susbscription
+        self.found_object_subscription
+
+        self.bounding_boxes = []
+        self.count_objects = 0
+
+
+    def boundingBoxesCallback(self, msg):
+        self.bounding_boxes = msg.bounding_boxes
+
+    def getBoundingBoxList(self):
+        return self.bounding_boxes
+    
+    def foundObjectCallback(self, msg):
+        self.count_objects = msg.count
+    
+    def getNumObjects(self):
+        return self.count_objects
+
+
+class Laser(Node):
+    
+    def __init__(self):
+        super().__init__("laser_node")
+        
+        self.laser_subscription = self.create_subscription(
+            LaserScan,
+            "scan",
+            self.laserScanCallback,
+            10)
+        
+        self.laser_subscription
+
+        self.laser_data = None
+    
+    def laserScanCallback(self, msg):
+        self.laser_data = msg
+    
+    def getLaserData(self):
+        return self.laser_data
+
+
 
 rclpy.init()
 velocity_node = VelocityPublisher()
+darknet_ros_node = DarknetRos()
+laser_node = Laser()
+
 
 # -- HAL Functions --
 #####################
@@ -53,6 +115,20 @@ def setV(num):
 def setW(num):
     velocity_node.setW(num)
 
+def getBoundingBoxes():
+    return darknet_ros_node.getBoundingBoxList()
+
+def getNumObjects():
+    return darknet_ros_node.getNumObjects()
+
+def getLaserData():
+    ldata = laser_node.getLaserData()
+    if ldata != None:
+        v1 = ldata.ranges[90:0:-1]
+        v1.append(ldata.ranges[0])
+        v2 = ldata.ranges[359:270:-1]
+        return v1+v2
+    return None
 
 
 def main(user_main, num_threads=4, args=None):
@@ -60,6 +136,8 @@ def main(user_main, num_threads=4, args=None):
     executor = MultiThreadedExecutor(num_threads=num_threads)
 
     executor.add_node(velocity_node)
+    executor.add_node(darknet_ros_node)
+    executor.add_node(laser_node)
     try:
         while rclpy.ok():
             user_main()
--- follow_person/follow_person/follow_person_main.py
+++ follow_person/follow_person/follow_person_main.py
@@ -4,13 +4,57 @@ import follow_person.HAL as HAL
 # ----- USER PROGRAM -----
 
 v = 0.3
-HAL.setV(v)
+HAL.setV(0)
 HAL.setW(0)
 
+def getCentroid(bounding_box):
+    xmin = bounding_box.xmin
+    xmax = bounding_box.xmax
+    ymin = bounding_box.ymin
+    ymax = bounding_box.ymax
+    return ((xmin + xmax)/2.0, (ymin + ymax)/2.0)
+
+
+def getArea(bounding_box):
+    xmin = bounding_box.xmin
+    xmax = bounding_box.xmax
+    ymin = bounding_box.ymin
+    ymax = bounding_box.ymax
+    return ((xmax - xmin) * (ymax - ymin))
+
+
+def maxAreaBoundingBox(bounding_boxes, class_id):
+    imax = 0
+    detection = False
+    for i in range(len(bounding_boxes)):
+        if bounding_boxes[i].class_id == class_id:
+            detection = True
+            area = getArea(bounding_boxes[i])
+            if area > getArea(bounding_boxes[imax]):
+                imax = i
+    if not detection:
+        return None
+    return bounding_boxes[imax]
+
+
 def user_main(args=None):
-    global v
-    v += 0.001
-    HAL.setV(v)
+    # bounding_boxes = HAL.getBoundingBoxes()
+
+    # if HAL.getNumObjects() != 0:
+    #     for bounding_box in bounding_boxes:
+    #         if bounding_box.class_id == "person":
+    #             cx, cy = getCentroid(bounding_box)
+    #             print("persona detectada en ({},{}) y area {}".format(cx, cy, getArea(bounding_box)))
+    #     max_bounding_box = maxAreaBoundingBox(bounding_boxes, 'person')
+    #     if max_bounding_box != None:
+    #         print("maximo area: ", getArea(max_bounding_box))
+
+    laser_data = HAL.getLaserData()
+    if laser_data != None:
+        for i in range(len(laser_data)):
+            print("["+str(i)+"]:", laser_data[i], end=' ')
+        print("\n\n")
+    #HAL.setW(0.2)
 
 ##########################
 
